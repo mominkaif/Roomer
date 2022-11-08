70c01f5d29db824d050b41d0448b8e5ed19a50f6
mominkaif <kaifmomin30@gmail.com>
2022-11-07 23:48:25 -0500

    updates to authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ProfilesContextProvider } from './context/ProfileContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProfilesContextProvider>
-      <App />
-    </ProfilesContextProvider>
+    <AuthContextProvider>
+      <ProfilesContextProvider>
+        <App />
+      </ProfilesContextProvider>
+    </AuthContextProvider>
+    
   </React.StrictMode>
 );
