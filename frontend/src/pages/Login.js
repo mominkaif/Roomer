e71af82c82ae92e5950e59eb4d71b93fbd803176
Kaif Momin <kaifmomin30@gmail.com>
2023-01-10 15:53:59 -0500

    fixed up forms

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,7 +5,7 @@ import { useNewProfileContext } from '../hooks/useNewProfileContext'
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {login, error, isLoading} = useLogin()
+    const { login, error, isLoading } = useLogin()
 
     const { newDispatch } = useNewProfileContext()
 
@@ -13,35 +13,39 @@ const Login = () => {
         e.preventDefault()
         let userCreated = await login(email, password)
         if (userCreated) {
-            await newDispatch({type: 'CREATED_PROFILE', payload: true})
+            await newDispatch({ type: 'CREATED_PROFILE', payload: true })
             localStorage.setItem('created_profile?', JSON.stringify(true))
         }
     }
 
-    return(
-        <form className="login" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
+    return (
+        <div className="form">
+            <form className="login" onSubmit={handleSubmit}>
+                <h1>Roomer</h1>
 
-            <label>Email:</label>
-            <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
 
-            <label>Password:</label>
-            <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
 
-            <button disabled={isLoading}>Log in</button>
-            <p>
-                Don't have an account: <a href="/signup">signup</a> here
-            </p>
-            {error && <div className="error">{error}</div>}
-        </form>
+                <button disabled={isLoading}>Log in</button>
+                <p>
+                    Don't have an account: <a href="/signup">signup</a> here
+                </p>
+                {error && <div className="error">{error}</div>}
+            </form>
+        </div>
+
+
 
     )
 }
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useSignup } from "../hooks/useSignup"
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
     const [email, setEmail] = useState('')
@@ -21,29 +21,31 @@ const Signup = () => {
     }
 
     return (
-        <form className="signup" onSubmit={handleSubmit}>
-            <h3>Sign up</h3>
+        <div className="form">
+            <form className="signup" onSubmit={handleSubmit}>
+                <h1>Roomer</h1>
 
-            <label>Email:</label>
-            <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
 
-            <label>Password:</label>
-            <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
 
-            <button disabled={isLoading}>Sign up</button>
-            <p>
-                Already have an account: <a href="/login">login</a> here
-            </p>
-            {error && <div className="error">{error}</div>}
-        </form>
+                <button disabled={isLoading}>Sign up</button>
+                <p>
+                    Already have an account: <a href="/login">login</a> here
+                </p>
+                {error && <div className="error">{error}</div>}
+            </form>
+        </div>
 
     )
 }
